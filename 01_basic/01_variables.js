cec3ff536f3bee29b1435d52d686cbea60d60e14
sunnyKush28 <152369021+sunnyKush28@users.noreply.github.com>
2024-06-20 11:58:25 +0000

    data type

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,6 @@
+const accountId =144553
+let accountEmail ="sunny@gmail.com"
+var accountPass = "123456"
+
+accountCity = "Indore"
+let accountSate;
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //not work on terminal its work on browser 
+
+
+let name = "Sunny" // String Data type
+let age = 18 //number Data type
+
